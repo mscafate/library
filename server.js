60d42c67fb527db8e77edd627e69dc42e17527ac
mscafate <michaelscafate@yahoo.com>
2021-08-12 14:17:16 -0400

    Books Create/Index/New part 3

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 
+const bookRouter = require('./routes/books')
+
 
 
 app.set('view engine', 'ejs')
@@ -23,7 +25,7 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, 
     useUnifiedTopology: true})
-    //mongodb+srv://mscafate:ms925808@cluster0.mmz3f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+    
 
 const db = mongoose.connection
 db.on('error', error => console.error(error))
@@ -31,5 +33,7 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
+
 
 app.listen(process.env.PORT || 3000)
